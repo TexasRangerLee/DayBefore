0d51ea2f140ce6994379758065634a463e8ba5f4
Clark Wagener <cpw8754@louisiana.edu>
2017-02-02 14:13:20 -0600

    Added in Pac-Man holder.
    Added in Pac-Man holder that works with a character controller. Numbers
    need to be tweeked, but additional things can be added to Pac-Man

---
+++ Pac-man/Assets/Clark's_Additions/Scripts/Camera_Follower.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_Follower : MonoBehaviour 
+{
+    [SerializeField]
+    GameObject PacMan;
+
+    [SerializeField]
+    float offset;
+
+    //Smooth following
+    [SerializeField]
+    public float distanceAwayFrom;
+    [SerializeField]
+    public float distanceUpFrom;
+    [SerializeField]
+    public float smoothCamera;
+    [SerializeField]
+    public Transform followPacMan;
+    private Vector3 targetsPosition;
+
+	// Use this for initialization
+	void Start () 
+    {
+        followPacMan = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //transform.position = new Vector3(PacMan.transform.position.x, PacMan.transform.position.y, PacMan.transform.position.z - offset);
+        //transform.rotation = PacMan.transform.rotation;
+	}
+
+    void LateUpdate()
+    {
+        targetsPosition = followPacMan.position + Vector3.up * distanceUpFrom - followPacMan.forward * distanceAwayFrom;
+
+        transform.position = Vector3.Lerp(transform.position, targetsPosition, Time.deltaTime * smoothCamera);
+
+        transform.LookAt(followPacMan);
+    }
+}
