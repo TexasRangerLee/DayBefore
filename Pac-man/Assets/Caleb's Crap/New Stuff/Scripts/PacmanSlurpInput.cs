3fac96d80026a9657d1a3471fd5c1293ea589ab6
calebgeorge3 <calebgeorge3@gmail.com>
2017-02-02 16:21:14 -0600

    Added my stuff
    added my pacman model and pellet models to scene; added small terrain
    square to keep my stuff out of the way of the patroller; will do more
    in-depth working of tags/layers next

---
+++ Pac-man/Assets/Caleb's Crap/New Stuff/Scripts/PacmanSlurpInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanSlurpInput : MonoBehaviour {
+
+    public GameObject topLip;
+    public GameObject bottomLip;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.P))
+        {
+            topLip.GetComponent<Animator>().SetTrigger("TriggerSlurp");
+            bottomLip.GetComponent<Animator>().SetTrigger("TriggerSlurp");
+        }
+	}
+}
