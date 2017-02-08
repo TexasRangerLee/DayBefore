4fc966f7252e31de2f64b1e821efd98f2d39c16c
calebgeorge3 <calebgeorge3@gmail.com>
2017-02-07 20:43:06 -0600

    Brought in menus and such; implemented infinite energy toggle

---
+++ Pac-man/Assets/Menu Scenes/MasterVolumeScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MasterVolumeScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //Used with the master volume slider
+    public void MasterVolumeChange(UnityEngine.UI.Slider slider)
+    { AudioListener.volume = slider.value; }
+}
